07f99b6f578650af3f554b55df2ca626015aee5c
darshan-tagline <darshanun.taglineinfotech@gmail.com>
2025-03-20 16:40:45 +0530

    update category changes

--- src/controller/categoryController.js
+++ src/controller/categoryController.js
@@ -62,8 +62,10 @@ const updateCategory = async (req, res) => {
     const { id } = req.params;
     const { name, description } = req.body;
     const alreadyExist = await findCategory({ name });
-    if (alreadyExist) {
-      return sendResponse(res, 400, "Category name already exists");
+    if (name) {
+      if (alreadyExist) {
+        return sendResponse(res, 400, "Category name already exists");
+      }
     }
     const updatedCategory = await modifyCategory(id, {
       name,
